84dd812af6257320b25d9dacaebaa0ad27ab4cdd
Ailuro <ailuroluna.l@gmail.com>
2024-01-30 15:16:17 +0100

    change the parameters, now only need generations, tau, k1, and k2 as inputs

--- code/ca_model.py
+++ code/ca_model.py
@@ -5,10 +5,18 @@ import random
 RANDOM_SEED = 42
 random.seed(RANDOM_SEED)
 
+ROWS = COLS = 101
+ORIGIN = (COLS // 2, ROWS // 2)
+PHI = 1000
+CANCER_INIT_POSITIONS = [(ORIGIN[0], ORIGIN[1]), (ORIGIN[0] + 1, ORIGIN[1]),
+                         (ORIGIN[0] - 1, ORIGIN[1]), (ORIGIN[0], ORIGIN[1] - 1),
+                         (ORIGIN[0], ORIGIN[1] + 1)]
+K3, K4 = 0.4, 0.4
+RHO = 3.85
 
-def initialize_grid(rows, cols, cancer_init_positions):
-    M = np.full((rows, cols), 'N')
-    for pos in cancer_init_positions:
+def initialize_grid():
+    M = np.full((ROWS, COLS), 'N')
+    for pos in CANCER_INIT_POSITIONS:
         M[pos] = 'C'
     return M
 
@@ -26,45 +34,45 @@ def calculate_n_prime(M):
     return n_prime
 
 
-def origin_distance(M, origin):
+def origin_distance(M):
     n_prime = calculate_n_prime(M)
     R = 0
     for i in range(len(M)):
         for j in range(len(M[0])):
             if M[i, j] == 'C':
-                R += math.sqrt((i - origin[0]) ** 2 + (j - origin[1]) ** 2)
+                R += math.sqrt((i - ORIGIN[0]) ** 2 + (j - ORIGIN[1]) ** 2)
     R = R / n_prime if n_prime else 0
     return R
 
 
-def density_development(M, origin):
+def density_development(M):
     """ Calculate density development of tumor. """
     n_prime = calculate_n_prime(M)
-    R = origin_distance(M, origin)
+    R = origin_distance(M)
     return n_prime / R ** 2 if R else 0
 
 
-def mitosis_probability(k, n, p, time_delay, generation, history):
+def mitosis_probability(k, n, time_delay, generation, history):
     delayed_gen = generation - time_delay
     if delayed_gen in history:
         n_delayed = history[delayed_gen]['Nc']
-        return k * (1 - n_delayed / p)
+        return k * (1 - n_delayed / PHI)
     else:
-        return k * (1 - n / p)
+        return k * (1 - n / PHI)
 
 
-def store_history(generation, M, history, origin, RHO):
+def store_history(generation, M, history):
     """ Store the number of each cell type and R at the current generation. """
     Nc = sum_cell_type(M, 'C')
     Ne = sum_cell_type(M, 'E')
     Nd = sum_cell_type(M, 'D')
-    R = origin_distance(M, origin)  # Calculate radius here
-    dense = (density_development(M, origin) > RHO)
+    R = origin_distance(M)  # Calculate radius here
+    dense = (density_development(M) > RHO)
     history[generation] = {'Nc': Nc, 'Ne': Ne, 'Nd': Nd, 'R': R, "dense": dense}
 
 
-def get_quadrant(r, c, ORIGIN):
-    """ Get quadrant of coordinates relative to origin. """
+def get_quadrant(r, c):
+    """ Get quadrant of coordinates relative to ORIGIN. """
     if r <= ORIGIN[0] and c > ORIGIN[1]:
         return 'I'
     elif r <= ORIGIN[0] and c <= ORIGIN[1]:
@@ -75,10 +83,10 @@ def get_quadrant(r, c, ORIGIN):
         return 'IV'
 
 
-def mitosis(M, newM, r, c, dense, ORIGIN):
+def mitosis(M, newM, r, c, dense):
     """ Model cell division with density development. """
     up, rt, dn, lt = (r - 1, c), (r, c + 1), (r + 1, c), (r, c - 1)
-    quadrant = get_quadrant(r, c, ORIGIN)
+    quadrant = get_quadrant(r, c)
     dense_map = {'I': [up, rt], 'II': [lt, up], 'III': [dn, lt], 'IV': [rt, dn]}
     not_dense_map = {'I': [dn, lt], 'II': [rt, dn], 'III': [up, rt], 'IV': [lt, up]}
     not_normal = ('E', 'D')
@@ -91,46 +99,46 @@ def mitosis(M, newM, r, c, dense, ORIGIN):
             break
     return newM
 
-def simulate_tumor_growth_one_step(M, generation, time_delay, history, phi, rho, k1, k2, k3, k4, origin, rows, cols):
+def simulate_tumor_growth_one_step(M, generation, time_delay, history, k1, k2):
     newM = np.copy(M)
-    store_history(generation, M, history, origin, rho)
+    store_history(generation, M, history)
     dense = history[generation]['dense']
 
     delayed_gen = generation - time_delay
     if delayed_gen in history:
         dense = history[delayed_gen]['dense']
 
-    for r in range(1, rows - 1):
-        for c in range(1, cols - 1):
+    for r in range(1, ROWS - 1):
+        for c in range(1, COLS - 1):
             if M[r, c] == 'C':
-                mitosis_prob = mitosis_probability(k1, sum_cell_type(M, 'C'), phi, time_delay, generation, history)
+                mitosis_prob = mitosis_probability(k1, sum_cell_type(M, 'C'), time_delay, generation, history)
                 if random.random() < mitosis_prob:
-                    newM = mitosis(M, newM, r, c, dense, origin)
+                    newM = mitosis(M, newM, r, c, dense)
                 elif random.random() < k2:
                     newM[r, c] = 'E'
-            elif M[r, c] == 'E' and random.random() < k3:
+            elif M[r, c] == 'E' and random.random() < K3:
                 newM[r, c] = 'D'
-            elif M[r, c] == 'D' and random.random() < k4:
+            elif M[r, c] == 'D' and random.random() < K4:
                 newM[r, c] = 'N'
     return newM
 
 
-def simulate_tumor_growth(time_delay, generations, rows, cols, phi, rho, k1, k2, k3, k4, cancer_init_positions, origin):
+def simulate_tumor_growth(time_delay, generations, k1, k2):
     history = {}
-    M = initialize_grid(rows, cols, cancer_init_positions)
+    M = initialize_grid()
 
     for g in range(generations):
-        M = simulate_tumor_growth_one_step(M, g, time_delay, history, phi, rho, k1, k2, k3, k4, origin, rows, cols)
+        M = simulate_tumor_growth_one_step(M, g, time_delay, history, k1, k2)
 
     return history
 
-def simulate_tumor_growth_with_clusters(time_delay, generations, rows, cols, phi, rho, k1, k2, k3, k4, cancer_init_positions, origin):
+def simulate_tumor_growth_with_clusters(time_delay, generations, k1, k2):
     history = {}
-    M = initialize_grid(rows, cols, cancer_init_positions)
+    M = initialize_grid()
     M_cluster = []
 
     for g in range(generations):
-        M = simulate_tumor_growth_one_step(M, g, time_delay, history, phi, rho, k1, k2, k3, k4, origin, rows, cols)
+        M = simulate_tumor_growth_one_step(M, g, time_delay, history, k1, k2)
 
         M_cluster.append(M)
 
